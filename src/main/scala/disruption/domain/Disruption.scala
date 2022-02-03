d51cd0991be6831e27cfaf16798c7ab11701d5e7
tammosminia <tammosminia@gmail.com>
2022-02-03 16:31:43 +0100

    asop

--- src/main/scala/disruption/domain/Disruption.scala
+++ src/main/scala/disruption/domain/Disruption.scala
@@ -1,14 +1,19 @@
 package disruption.domain
 
+import com.akkaserverless.scalasdk.SideEffect
 import com.akkaserverless.scalasdk.valueentity.ValueEntity
 import com.akkaserverless.scalasdk.valueentity.ValueEntityContext
 import com.google.protobuf.empty.Empty
 import disruption.api
+import disruption.api.Flight
 
 // This class was initially generated based on the .proto definition by Akka Serverless tooling.
 //
 // As long as this file exists it will not be overwritten: you can maintain it yourself,
 // or delete it so it is regenerated as needed.
+
+//api.Disruption en deze class komen uit disruption_api.proto
+//DisruptionState uit disruption_domain.proto
 
 /** A value entity. */
 class Disruption(context: ValueEntityContext) extends AbstractDisruption {
@@ -41,11 +46,26 @@ class Disruption(context: ValueEntityContext) extends AbstractDisruption {
       flight = d.flight
     )
 
-  override def flightUpdates(currentState: DisruptionState, flightUpdatesRequest: api.FlightUpdatesRequest): ValueEntity.Effect[api.Disruptions] =
-    effects.error("The command handler for `flightUpdates` is not implemented, yet")
+//  override def flightUpdates(currentState: DisruptionState, flightUpdatesRequest: api.FlightUpdatesRequest): ValueEntity.Effect[api.Disruptions] = {
+//    val ds = flightUpdatesRequest.flights.flatMap(flightToDisruption)
+//    val updates = ds.map { d =>
+//      //SideEffect moet een DeferredCall zijn (een functie van een Service)
+//      SideEffect(components.disruption.create(d))
+//    }
+//    val response = api.Disruptions(disruptions = ds)
+//    effects.reply(response).addSideEffects(updates)
+//  }
 
-  override def getAll(currentState: DisruptionState, empty: Empty): ValueEntity.Effect[api.Disruptions] =
-    effects.error("The command handler for `GetAll` is not implemented, yet")
+  private def flightToDisruption(f: Flight): Option[api.Disruption] = {
+    if (f.isCancelled) {
+      Some(new api.Disruption(eventType = "cancellation", flight = f.flightKey))
+    } else {
+      None
+    }
+  }
+
+//  override def getAll(currentState: DisruptionState, empty: Empty): ValueEntity.Effect[api.Disruptions] =
+//    effects.error("The command handler for `GetAll` is not implemented, yet")
 
 }
 
