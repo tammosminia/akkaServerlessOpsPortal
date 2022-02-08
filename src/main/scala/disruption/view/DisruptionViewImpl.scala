90466b344b7e37106b9c7d557c0837dcc8d59d0f
tammosminia <tammosminia@gmail.com>
2022-02-08 16:15:56 +0100

    asop

---
+++ src/main/scala/disruption/view/DisruptionViewImpl.scala
@@ -0,0 +1,17 @@
+package disruption.view
+
+import com.akkaserverless.scalasdk.view.View.UpdateEffect
+import com.akkaserverless.scalasdk.view.ViewContext
+import com.google.protobuf.empty.Empty
+import disruption.domain.DisruptionState
+
+// This class was initially generated based on the .proto definition by Akka Serverless tooling.
+//
+// As long as this file exists it will not be overwritten: you can maintain it yourself,
+// or delete it so it is regenerated as needed.
+
+class DisruptionViewImpl(context: ViewContext) extends AbstractDisruptionView {
+
+
+  
+}
